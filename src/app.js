e5c509774b9fddb90770a42eeb334426e5582848
Ben <bennnnnnnvu@gmail.com>
2024-03-25 15:42:09 -0700

    install cross-env package to set environment variables. Modify package.json to add a script to add an environment variable NODE_ENV=development when app is being built in development mode

--- src/app.js
+++ src/app.js
@@ -6,12 +6,13 @@ dotenv.config();
 
 //create express app
 const app = express();
+const port = process.env.port || 9000;
+
+// console.log(process.env.NODE_ENV); // verify that we are in development mode
 
 app.get("/test", (req, res) => {
     res.send("Hello from our backend server!");
 });
-
-const port = process.env.port || 9000;
 
 app.listen(port, () => console.log(`Server is listening on port ${port}!!!`));
 
